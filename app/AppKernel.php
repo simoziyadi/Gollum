d9c5047722174471b174c00ba604923cad022990
ZYADI Mohammed <simoziyadi@gmail.com>
2016-02-26 17:43:56 +0100

    New Bundle Directory

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new MIT\FinanceBundle\MITFinanceBundle(),
+            new MIT\DirectoryBundle\MITDirectoryBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/MIT/DirectoryBundle/MITDirectoryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MIT\DirectoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MITDirectoryBundle extends Bundle
+{
+}
