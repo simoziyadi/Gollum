1fff030fc8358604a1a645577ca92b4853434587
ZYADI Mohammed <simoziyadi@gmail.com>
2016-03-02 19:09:32 +0100

    creating Compte from

---
+++ src/MIT/DirectoryBundle/Form/CompteType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MIT\DirectoryBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('numero')
+            ->add('solde')
+            ->add('dateCreation', DateTimeType::class)
+            ->add('save','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MIT\DirectoryBundle\Entity\Compte'
+        ));
+    }
+
+    public function  getName()
+    {
+        return 'mit_directory_bundle_compte';
+    }
+}
