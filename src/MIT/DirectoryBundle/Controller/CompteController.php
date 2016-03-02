269d15e392fd8acef847b675c3e531d9e42698d6
ZYADI Mohammed <simoziyadi@gmail.com>
2016-03-02 19:11:58 +0100

    creating addCompte and updateCompte and removeCompte actions

--- src/MIT/DirectoryBundle/Controller/CompteController.php
+++ src/MIT/DirectoryBundle/Controller/CompteController.php
@@ -9,8 +9,11 @@
 namespace MIT\DirectoryBundle\Controller;
 
 
+use MIT\DirectoryBundle\Entity\Compte;
+use MIT\DirectoryBundle\Form\CompteType;
 use MIT\DirectoryBundle\MITDirectoryBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CompteController extends Controller
 {
@@ -19,6 +22,55 @@ class CompteController extends Controller
         $comptes=$em->getRepository('MITDirectoryBundle:Compte')->findAll();
         return $this->render('MITDirectoryBundle:Compte:listeComptesAction.html.twig',array('cptes'=>$comptes));
 
+    }
+    public function addCompteAction(Request $request){
+
+         $compte = new Compte();
+        $compte->setDateCreation(new \DateTime());
+        $form=$this->get('form.factory')->create(new CompteType(),$compte);
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $em=$this->getDoctrine()->getEntityManager();
+            $em->persist($compte);
+            $em->flush();
+            return $this->redirectToRoute('listesComptes');
+        }
+        return $this->render('MITDirectoryBundle:Compte:addCompte.html.twig',array(
+            'form'=>$form->createView(),
+        ));
+
+
 
     }
+    public function updateCompteAction(Request $request,$id)
+    {
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $compte = $em->getRepository('MITDirectoryBundle:Compte')->find($id);
+        $form = $this->get('form.factory')->create(new CompteType(), $compte);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($compte);
+            $em->flush();
+            return $this->redirectToRoute('listesComptes');
+        }
+        return $this->render('MITDirectoryBundle:Compte:updateCompte.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+        public function removeCompteAction(Request $request,$id)
+        {
+            $em = $this->getDoctrine()->getEntityManager();
+            $compte = $em->getRepository('MITDirectoryBundle:Compte')->find($id);
+            $em->remove($compte);
+            $em->flush();
+            return $this->listComptesAction();
+        }
+
+
+
+
+
+
 }
